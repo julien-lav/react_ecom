adfa1fb1ceb65ba6d796ad6f29e80ab91dc6a675
Julien Laville <j.laville@claravista.dom>
2022-08-13 23:29:27 +0200

    Initial commit

---
+++ backend/data.js
@@ -0,0 +1,47 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Nike Slim Shirt',
+            category: 'Shirts',
+            image: '/images/michele-morgan.jpg',
+            price: 120,
+            countInStock: 1,
+            brand: 'Nike',
+            rating: 3.8,
+            numReviews: 10,
+            description: 'High quality product',
+
+
+        },
+        {
+            _id: '2',
+            name: 'Adidas Slim Shirt',
+            category: 'Shirts',
+            image: '/images/michele-morgan.jpg',
+            price: 90,
+            countInStock: 10,
+            brand: 'Adidas',
+            rating: 5,
+            numReviews: 2,
+            description: 'High quality product',
+
+
+        },
+        {
+            _id: '3',
+            name: 'Adidas Shirt Grey',
+            category: 'Shirts',
+            image: '/images/michele-morgan.jpg',
+            price: 60,
+            countInStock: 10,
+            brand: 'Adidas',
+            rating: 4,
+            numReviews: 2,
+            description: 'High quality product',
+
+
+        },
+    ]
+}
+export default data;
